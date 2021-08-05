2e3606cdb553e402b926aac7f327d9789a3576f6
debankur-saha <83601145+debankur-saha@users.noreply.github.com>
2021-08-05 23:07:36 +0530

    Update Activity_03.py

--- Activity_03.py
+++ Activity_03.py
@@ -2,5 +2,7 @@ string1=input("Enter 1st word: ")
 string2=input("Enter 2nd word: ")
 output1=(string1,string2)
 output2=(string1,string2)*5
+output3=(string1, "\t", string2, "\n")*5
 print(*output1,sep='')
 print(*output2,sep='')
+print(*output3, sep='')
