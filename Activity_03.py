ab388cfee358eb5d2b1646ae297d663f7d74d1e1
debankur-saha <83601145+debankur-saha@users.noreply.github.com>
2021-08-05 22:40:01 +0530

    Update Activity_03.py

--- Activity_03.py
+++ Activity_03.py
@@ -1,5 +1,6 @@
 string1=input("Enter 1st word: ")
 string2=input("Enter 2nd word: ")
-output=(string1,string2)*5
-print(string1,string2)
-print(*output,sep='')
+output1=(string1,string2)
+output2=(string1,string2)*5
+print(*output1,sep='')
+print(*output2,sep='')
