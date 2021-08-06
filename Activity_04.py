4cd7d2252b18efd13e50328a477cbe529d64adb2
debankur-saha <83601145+debankur-saha@users.noreply.github.com>
2021-08-06 10:39:29 +0530

    Update Activity_04.py

--- Activity_04.py
+++ Activity_04.py
@@ -1,4 +1,7 @@
-num1=input("Enter 1st number=")
-num2=input("Enter 2nd number=")
+num1=float(input("Enter 1st number="))
+num2=float(input("Enter 2nd number="))
 sum=num1+num2
 print(num1,"+",num2,"=",sum)
+print("%f + %f = %f"%(num1,num2,sum))
+print(sum)
+print('{}+{}={}'.format(num1,num2,sum))
